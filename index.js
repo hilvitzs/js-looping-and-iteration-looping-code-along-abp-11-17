18185290ce7d4d9ccecce21569bbb75ab3dffb36
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2017-11-09 21:37:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for(let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`)
+  }
+
+  return names;
+}
+
+function maybeTails() {
+  Math.random() >= 5;
+}
+
+while (maybeTails()) {
+  let times = 1;
+  times++;
+}
